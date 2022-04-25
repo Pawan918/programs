5b268a0d6e012c5428afe2399959a00ea80566a2
pawan918 <pawanshah84489@gmail.com>
2022-04-26 00:23:30 +0530

    26/04/22

--- Elevator Rides/index.cpp
+++ Elevator Rides/index.cpp
@@ -8,17 +8,17 @@ int main(){
     vector<int> elevator(n);
     for(int i=0;i<n;i++)
         cin>>elevator[i];
-
+    sort(elevator.begin(),elevator.end());
     vector<int> s;
-    s.push_back(elevator[0]);
-    for(int i=1;i<n;i++){
-        int find = weight-elevator[i];
-        auto it = upper_bound(s.begin(),s.end(),find);
-        if(*it >= s.size()){
-            s.push_back(elevator[i]);
+    s.push_back(weight-elevator[n-1]);
+    for(int i=n-2;i>=0;i--){
+        auto it = lower_bound(s.begin(),s.end(),elevator[i])-s.begin();
+        if(it < s.size()){
+            s[it] = s[it] -elevator[i];
         }else{
-            s[*it] += elevator[i];
+            s.push_back(weight-elevator[i]);
         }
+        sort(s.begin(),s.end());
     }
     cout<<s.size();
     return 0;
